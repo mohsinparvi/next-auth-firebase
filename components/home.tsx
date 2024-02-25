5f960df265df6daa444f2ae70be1f937a4f5e764
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2024-02-25 12:38:35 +0500

    fix: home page design issue

--- components/home.tsx
+++ components/home.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold">Home page</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
